96fb0ec18932ace322467f3dc83d23b4466c6cc2
Whittaker, Derik <DerikWhittaker@quickenloans.com>
2019-01-11 06:57:26 -0500

    small formatting, added try/catch

--- src/index.js
+++ src/index.js
@@ -1,21 +1,49 @@
 const logger = require('./lib/logger')
-
 const leagueTableService = require('./services/leagueTableService')
 
+/**
+ * Handler which is invoked by triggering events
+ * 
+ * @returns {object}
+ '''
+ {
+    statusCode: 200 || ???,
+    body: [
+        {
+            losses: number,
+            teamName: string,
+            id: number,
+            wins: number,
+            draws: number,
+            points: number
+        }
+    ]
+ }
+ '''
+ */
 async function handler() {
     logger.info('Index.handler', 'Attempting to startup Handler');
 
-    const teamsResults = await leagueTableService.teamsByPoints();
+    try {
+        const teamsResults = await leagueTableService.teamsByPoints();
 
-    logger.debug('Index.handler', `Teams Results: ${JSON.stringify(teamsResults)} `);
+        logger.trace('Index.handler', `Teams Results: ${JSON.stringify(teamsResults)} `);
 
-    var response = {
-        "statusCode": 200,
-        "body": JSON.stringify(teamsResults)
-    };
+        var response = {
+            "statusCode": 200,
+            "body": JSON.stringify(teamsResults)
+        };
 
-    return response;
+        return response;
+    } catch (error) {
+        logger.info('index.handler', JSON.stringify(error));
+
+        return {
+            statusCode: 400,
+            error: error
+        }
+    }
+
 }
-
 
 exports.handler = handler
